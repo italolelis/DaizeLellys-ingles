9d78b679b7cd273210ccd155be019af35aba14a8
Erica <erica.l.s1996@gmail.com>
2026-06-26 23:05:57 +0200

    subindo atualizaçoes de responsividade

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,6 @@ import React from 'react'
 import Carrossel from '../../components/Carrossel/Carrossel'
 import SobreMim from '../../components/SobreMim/SobreMim'
 import Footer from '../../components/Footer/Footer'
-import Motivacao from '../../components/Motivacao/Motivacao'
 import PortuguesEstrangeiro from '../../components/PortuguesEstrangeiro/PortuguesEstrangeiro'
 
 import { Youtube } from '../../components/Youtube/Youtube'
@@ -20,7 +19,7 @@ const Home = () => {
         <WalktheTalk></WalktheTalk>
         <PortuguesEstrangeiro></PortuguesEstrangeiro>
         {/* <Comentarios></Comentarios> */}
-        <Motivacao></Motivacao>
+        
         <Youtube></Youtube>
         {/* <Ebook></Ebook> */}
         <Footer></Footer>
